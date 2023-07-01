10284c4efcd8acff133b409f2ee93865d907f8fa
gabrielskz <138263263+gabrielskz@users.noreply.github.com>
2023-06-30 22:48:52 -0300

    Trabalho 3

---
+++ styles.cs
@@ -0,0 +1,68 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #f0f0f0;
+    padding: 10px;
+}
+
+header h1 {
+    margin: 0;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 10px;
+}
+
+nav ul li a {
+    text-decoration: none;
+}
+
+main {
+    padding: 20px;
+}
+
+section {
+    margin-bottom: 20px;
+}
+
+section h2 {
+    margin-top: 0;
+}
+
+form label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+form input[type="text"],
+form input[type="email"],
+form input[type="tel"] {
+    width: 100%;
+    margin-bottom: 10px;
+    padding: 5px;
+}
+
+form button[type="submit"] {
+    padding: 5px 10px;
+}
+
+footer {
+    background-color: #f0f0f0;
+    padding: 10px;
+    text-align: center;
+}
+
+.redes-sociais img {
+    width: 20px;
+    margin-right: 5px;
+}
